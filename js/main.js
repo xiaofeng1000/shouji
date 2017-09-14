bc4eb80de40f75a18e62170ce4f11a28856237b9
xiaofeng1000 <1435425451@qq.com>
2017-09-14 15:33:01 +0800

    aaa

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 $(function(){
+	$('.m-brand ul li').eq(4).after('<li class="m-ioc"><i class="fa fa-angle-down fa-lg"></i></li>');
 	var _mIoc = $('li.m-ioc').nextAll();
 	_mIoc.hide();
 	$('li.m-ioc').click(function(){
