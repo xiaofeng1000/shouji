625a1f43d88d5eee0c5ea538dc556906ee14723d
xiaofeng1000 <1435425451@qq.com>
2017-09-27 18:59:27 +0800

    a

--- js/main.js
+++ js/main.js
@@ -12,10 +12,36 @@ $(function(){
 		$(this).next('ul.fault-info-mes-tow').slideToggle(500);
 
 	})
+	
 	$('.address').click(function(){
 		$('.city').show();
+		var con ='';
+		$.ajax({
+			type:'GET',
+			dataType: 'json',
+			url:'js/city.json',
+			success: function(data){
+				// console.log(data.json());
+				$.each(data.province,function(i,n){
+					con +="<li data-id="+n.provinceid+">"+n.provincename+"</li>";
+				});
+				$('.city-s').html(con);
+				$('.city-s >li').click(function(){
+					$(this).addClass('city-the').siblings().removeClass('city-the');
+					
+				});			
+  			 }
+		});
 	});
+	// $('.city-s').html(con);
+	// var ll ="<li data-id="+n.provinceid+">"+n.provincename+"</li>";
+	// console.log(con);
+	// $('.city-s >li').click(function(){
+	// 	// $(this).addClass('city-the');
+	// 	// console.log(this);
+	// });
 	$('.city-top >span').click(function(){
 		$('.city').hide();
 	})
+
 });
