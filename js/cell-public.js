d6409c82a814805b44c14ed83fd8f5c6e8e8e1fe
xiaofeng1000 <1435425451@qq.com>
2017-10-18 10:37:31 +0800

    手机维修dome

--- js/cell-public.js
+++ js/cell-public.js
@@ -4,37 +4,6 @@ var callback = function(city){
   cityJson = city;
 }
 $(function(){
-	
-	var float=$(".float");
-	var divs=$(".box div");
-	var num1=0;
-	var num2=0;
-	
-	$(".leftB").click(function(){
-		divs.finish();
-		float.stop(true);
-		var temp=num1;
-		num1--;
-		if(num1==-1){
-			num1=2;
-		}
-		divs.eq(num1).css("left",320).animate({left:0});
-		divs.eq(temp).animate({left:-320});
-		
-	
-	});
-	$(".rightB").click(function(){
-		divs.finish();
-		float.stop(true);
-		var temp=num1;
-		num1++;
-		if(num1==3){
-			num1=0;
-		}
-		divs.eq(num1).css("left",-320).animate({left:0});
-		divs.eq(temp).animate({left:320});
-		
-	});
 	$('.select-input').click(function(){
 		if ($(this).hasClass('on')) {
 			$(this).removeClass('on').addClass('off');
@@ -42,10 +11,6 @@ $(function(){
 			$(this).removeClass('off').addClass('on');
 		}
 	
-	});
-	$('.cell-footer').find('b').text($('#cell-model').val());
-	$('#cell-model').change(function(){
-		$('.cell-footer').find('b').text($(this).val());
 	});
 	$.region();
 	$('#cell-submit').click(function(){
